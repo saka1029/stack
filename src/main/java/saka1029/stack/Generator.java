9b737bb26b3bc3dc58b673ba98813949787929cb
saka1029 <saka1029@gmail.com>
2024-01-11 23:21:44 +0900

    Update Generator.java

--- src/main/java/saka1029/stack/Generator.java
+++ src/main/java/saka1029/stack/Generator.java
@@ -14,39 +14,51 @@ public class Generator implements List {
         return new Generator(origin.child(), list);
     }
     
-    class Iter implements Iterator {
-        
-        Instruction current;
-        
-        Iter() {
-            context.execute(code);
-            this.current = advance();
-        }
-        
-        Instruction advance() {
-            Terminal t = context.run();
-            switch (t) {
-                case END:
-                    return null;
-                case YIELD:
-                    return context.pop();
-                default:
-                    throw new RuntimeException("Unsupported terminator '%s'".formatted(t));
-            }
-        }
-
-        @Override
-        public Instruction next() {
-            Instruction result = current;
-            if (result != null)
-                current = advance();
-            return result;
-        }
-    }
+//    class Iter implements Iterator {
+//        
+//        Instruction current;
+//        
+//        Iter() {
+//            context.execute(code);
+//            this.current = advance();
+//        }
+//        
+//        Instruction advance() {
+//            Terminal t = context.run();
+//            switch (t) {
+//                case END:
+//                    return null;
+//                case YIELD:
+//                    return context.pop();
+//                default:
+//                    throw new RuntimeException("Unsupported terminator '%s'".formatted(t));
+//            }
+//        }
+//
+//        @Override
+//        public Instruction next() {
+//            Instruction result = current;
+//            if (result != null)
+//                current = advance();
+//            return result;
+//        }
+//    }
 
     @Override
     public Iterator iterator() {
-        return new Iter();
+//        return new Iter();
+        context.execute(code);
+        return new Iterator() {
+            @Override
+            public Instruction next() {
+                Terminal t = context.run();
+                return switch (t) {
+                    case END -> null;
+                    case YIELD -> context.pop();
+                    default -> throw new RuntimeException("Unsupported terminator '%s'".formatted(t));
+                };
+            }
+        };
     }
     
     @Override
