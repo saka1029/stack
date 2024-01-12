80fec177285a4ec36ff8e3731d8043ff349f646f
saka1029 <saka1029@gmail.com>
2024-01-12 12:59:01 +0900

    Create TestGenerator.java

---
+++ src/test/java/test/saka1029/stack/TestGenerator.java
@@ -0,0 +1,14 @@
+package test.saka1029.stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGenerator {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
