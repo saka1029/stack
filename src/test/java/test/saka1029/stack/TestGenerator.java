14a85db25007663efa08220aa3ebbef86b7fc2d2
saka1029 <saka1029@gmail.com>
2024-01-13 20:40:29 +0900

    Update TestGenerator.java

--- src/test/java/test/saka1029/stack/TestGenerator.java
+++ src/test/java/test/saka1029/stack/TestGenerator.java
@@ -1,26 +1,30 @@
 package test.saka1029.stack;
 
 import static org.junit.Assert.assertEquals;
+import static saka1029.stack.Stack.context;
+import static saka1029.stack.Stack.eval;
 
 import org.junit.Test;
 
 import saka1029.stack.Context;
 import saka1029.stack.Generator;
-import saka1029.stack.Int;
-import saka1029.stack.List;
-import saka1029.stack.Stack;
 import saka1029.stack.Symbol;
 import saka1029.stack.Terminal;
 
 public class TestGenerator {
 
+    /**
+     * generator:
+     * ARGUMENT LIST generator -> LIST
+     */
     @Test
     public void test() {
-        Context c = Stack.context();
+        Context c = context();
         c.variable(Symbol.of("generator"), x -> x.push(Generator.of(x, c.pop(), c.pop())));
         c.variable(Symbol.of("yield"), Terminal.YIELD);
-        assertEquals(List.of(Int.of(1), Int.of(2), Int.of(3)),
-            Stack.eval(c, "'() '() '(1 2 + yield 2 yield 1 yield) generator 'rcons for"));
+        assertEquals(eval(c, "'(1 2 3)"),
+            eval(c, "'() '() '(1 2 + yield 2 yield 1 yield) generator 'rcons for"));
+        assertEquals(eval(c, "'(4 5 6)"),
+            eval(c, "'() 3 '(dup 1 2 + + yield dup 2 + yield dup 1 + yield) generator 'rcons for"));
     }
-
 }
