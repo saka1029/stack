597269ce630ff3aecc71b35fc10bd348ca45e50f
saka1029 <saka1029@gmail.com>
2024-01-15 16:13:25 +0900

    Update TestGenerator.java

--- src/test/java/test/saka1029/stack/TestGenerator.java
+++ src/test/java/test/saka1029/stack/TestGenerator.java
@@ -18,7 +18,8 @@ public class TestGenerator {
     public void test() {
         Context c = Stack.context();
         c.variable(Symbol.of("generator"), x -> x.push(Generator.of(x, c.pop())));
-        c.variable(Symbol.of("yield"), x -> x.push(Terminal.YIELD));
+//        c.variable(Symbol.of("yield"), x -> x.push(Terminal.YIELD));
+        c.variable(Symbol.of("yield"), Terminal.YIELD);
         assertEquals(List.of(Int.of(1), Int.of(2), Int.of(3)),
             Stack.eval(c, "'() '(1 2 + yield 2 yield 1 yield) generator 'rcons for"));
     }
