5d596fdb1a8edbff31fd9dcedcdd0b983058fcc0
saka1029 <saka1029@gmail.com>
2026-04-22 02:37:53 +0900

    ..

--- src/test/java/test/saka1029/stack/TestGenerator.java
+++ src/test/java/test/saka1029/stack/TestGenerator.java
@@ -18,10 +18,6 @@ public class TestGenerator {
     @Test
     public void testGenerator() {
         Context c = context();
-        // c.variable(Symbol.of("generator"), x -> x.push(Generator.of(x, c.pop())));
-        // c.variable(Symbol.of("generator1"), x -> x.push(Generator.of(x, c.pop(), c.pop())));
-        // c.variable(Symbol.of("generator2"), x -> x.push(Generator.of(x, c.pop(), c.pop(), c.pop())));
-        // c.variable(Symbol.of("yield"), Terminal.YIELD);
         assertEquals(eval(c, "'(3 2 1)"), eval(c, "'(1 2 + yield 2 yield 1 yield) generator"));
         assertEquals(eval(c, "'(6 5 4)"), eval(c, "3 '(dup 1 2 + + yield dup 2 + yield dup 1 + yield) generator1"));
         assertEquals(eval(c, "'(10 9 8)"), eval(c, "'3 4 '(+ dup 2 1 + + yield dup 2 + yield dup 1 + yield) generator2"));
